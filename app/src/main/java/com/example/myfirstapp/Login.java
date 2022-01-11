242fbc42974d3c8ccb94ca718af5feeacf63291f
gooriiie <fkstldghks123@naver.com>
2022-01-11 11:29:47 +0900

    feat : Login UI

---
+++ app/src/main/java/com/example/myfirstapp/Login.java
@@ -0,0 +1,49 @@
+package com.example.myfirstapp;
+
+import static android.widget.Toast.makeText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Login extends AppCompatActivity {
+
+    TextInputEditText id, password;
+    Button btn_login, btn_findPassword, btn_signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        id = findViewById(R.id.inputId);
+        password = findViewById(R.id.inputId);
+        btn_login = findViewById(R.id.button_login);
+        btn_findPassword = findViewById(R.id.button_findPassword);
+        btn_signup = findViewById(R.id.button_signup);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                intent.putExtra("id", id.getText().toString());
+                intent.putExtra("password", password.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btn_findPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                FindPassword로 intent
+            }
+        });
+    }
+}
